9051c01be9bfc251456d9dc341369bb65ddce920
Nick Scofield <c.nick.scofield@gmail.com>
2023-01-29 05:56:50 -0700

    commit tree 949fa9fca93a136198a39ff487de78b8c73fb96d

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
 # Your code here!
+
+def myFunction param
+    puts "Running myFunction"
+    param + 1
+end
+
+def greet_programmer
+    print "Hello, programmer!"
+end
+
+def greet name
+    print "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    print "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num1
+    return nil unless num1.class == Integer
+    num1 / 2
+end
